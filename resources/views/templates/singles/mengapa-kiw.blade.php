badc653c00fb41ecb0c84182906585c1af451b1e
Daniel <danielbekool@gmail.com>
2025-05-27 13:21:30 +0700

    mengapa kiw

--- resources/views/templates/singles/mengapa-kiw.blade.php
+++ resources/views/templates/singles/mengapa-kiw.blade.php
@@ -7,7 +7,7 @@
  @endPush
 
 @push('before_body_close')
-<script>AOS.init();</script>
+<script src="{{ asset('js/aos-animate.js') }}"></script>
  @endPush
 
 <x-layouts.app :title="$title ?? 'Default Page'" :body-classes="$bodyClasses">
